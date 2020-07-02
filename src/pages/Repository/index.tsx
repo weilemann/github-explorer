dc2adda773bb3ecd045303ef19cd02723b69127b
Yuri Weilemann <yuri_weilemann@hotmail.com>
2020-07-02 17:09:13 -0300

    Creating repositories detailed screen - developing issues list

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,90 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronsLeft, FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  useEffect(() => {
+    // api.get(`/repos/${params.repository}`).then((response) => {
+    //   console.log(response.data);
+    // });
+
+    // api.get(`/repos/${params.repository}/issues`).then((response) => {
+    //   console.log(response.data);
+    // });
+
+    async function loadData(): Promise<void> {
+      const [repository, issues] = await Promise.all([
+        api.get(`/repos/${params.repository}`),
+        api.get(`/repos/${params.repository}/issues`),
+      ]);
+
+      console.log(repository);
+      console.log(issues);
+    }
+
+    loadData();
+  }, [params.repository]);
+
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronsLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars1.githubusercontent.com/u/55397252?v=4"
+            alt="Yuri Lindão"
+          />
+          <div>
+            <strong>weilemann/pokedex</strong>
+            <p>descrição do repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1080</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertos</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="sdsddf">
+          <div>
+            <strong>fsafsadsa</strong>
+            <p>sdsadasdsa</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
